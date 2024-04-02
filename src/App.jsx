29a74f1db587ca65a39fe366f5429dadd67cb450
Cooper Spinelli <cooperspinelli@gmail.com>
2024-04-02 12:16:23 -0600

    Add starter files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import EightBall from "./EightBall"
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+  return (<div>
+    <EightBall />
+  </div>)
+};
+
+export default App;
